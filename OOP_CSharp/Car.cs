0acb3e1e7f6e7b51a7104de8055cc2896bdccbaf
Jose Laurito <jigolaurito93@yahoo.com>
2026-02-06 05:36:04 -0500

    first commit

---
+++ OOP_CSharp/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_CSharp
+{
+    internal class Car
+    {
+        //Constructor
+        public Car()
+        {
+            Console.WriteLine("An object of Car is created!");
+        }
+    }
+}
